62f56f9c23b9fbeda3365a5c1a691607fe3afe06
parkgo0504 <75320567+parkgo0504@users.noreply.github.com>
2023-04-28 06:58:58 +0900

    1

--- 1.py
+++ 1.py
@@ -1,9 +1,10 @@
 #Quiz) 변수를 이용하여 다음 문장을 출력하시오
 
-station = "사당"
+station = "사당"    
 print(station +"행 열차가 들어오고 있습니다")
 station = "신도림"
 print(station +"행 열차가 들어오고 있습니다")
 station = "인천공항"
 print(station +"행 열차가 들어오고 있습니다")
 
+print("showmethemoney")
